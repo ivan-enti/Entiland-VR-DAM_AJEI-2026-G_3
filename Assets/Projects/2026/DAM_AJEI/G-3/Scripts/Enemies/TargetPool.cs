27994933cdd5bc67e07caaf4354084d37188865c
ivan-enti <ivangonzalez3009@gmail.com>
2026-04-14 17:38:26 +0200

    Random Enemy Spawn

--- Assets/Projects/2026/DAM_AJEI/G-3/Scripts/Enemies/TargetPool.cs
+++ Assets/Projects/2026/DAM_AJEI/G-3/Scripts/Enemies/TargetPool.cs
@@ -5,7 +5,7 @@ namespace EntilandVR.DosCinco.DAM_AJEI.G_Tres
 {
     public class TargetPool : MonoBehaviour
     {
-        [SerializeField] private GameObject obj_to_pool;
+        [SerializeField] private List<GameObject> obj_to_pool = new List<GameObject>();
         [SerializeField] private int pool_num;
 
         private List<Target> pool = new List<Target>();
@@ -14,7 +14,7 @@ namespace EntilandVR.DosCinco.DAM_AJEI.G_Tres
         {
             for(int i = 0; i < pool_num; i++)
             {
-                GameObject obj = Instantiate(obj_to_pool);
+                GameObject obj = InstantiateRandomEnemy();
                 obj.SetActive(false);
                 pool.Add(obj.GetComponent<Target>());
             }
@@ -35,12 +35,19 @@ namespace EntilandVR.DosCinco.DAM_AJEI.G_Tres
 
             if(get_obj == null)
             {
-                get_obj = Instantiate(obj_to_pool);
+                get_obj = InstantiateRandomEnemy();
                 pool.Add(get_obj.GetComponent<Target>());
             }
 
             get_obj.SetActive(true);
             return get_obj;
+        }
+
+        public GameObject InstantiateRandomEnemy()
+        {
+            int rand = Random.Range(0, obj_to_pool.Count - 1);
+
+            return obj_to_pool[rand];
         }
     }
 }
